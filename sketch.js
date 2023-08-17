fec237e2e3e797654b8f18d9418bc9c8310fa082
Mumlemads <mads.lauge8@gmail.com>
2023-08-17 22:50:59 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,29 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1920, 960);
 }
 
 function draw() {
   background(220);
+}
+
+class Ship {
+  constructor(lives){
+    this.lives = lives;
+  }
+  /*if lives == 0 *die* */
+
+}
+
+class bunker {
+  constructor(lives){
+    this.lives = lives;
+  }
+  /*if lives == 0 *die* */
+}
+
+class alien {
+  constructor(lives){
+    this.lives = lives;
+  }
+  /*if lives == 0 *die* */
 }
