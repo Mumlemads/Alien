594b1038361673fa28327dcafc83ced4711f95ec
Mumlemads <mads.lauge8@gmail.com>
2023-08-21 13:44:48 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,29 +1,39 @@
 function setup() {
-  createCanvas(1920, 960);
+  createCanvas(1280, 720);
+  background(0,0,0);
+  player = new Ship();
+  b1 = new bunker();
+  b2 = new bunker();
+  b3 = new bunker();
 }
 
 function draw() {
-  background(220);
+  background(0);
 }
 
 class Ship {
-  constructor(lives){
+  constructor(lives = 3, x = width/2, y = 10){
     this.lives = lives;
+    this.x = x;
+    this.y = y;
   }
   /*if lives == 0 *die* */
 
 }
 
 class bunker {
-  constructor(lives){
+  constructor(lives = 40){
     this.lives = lives;
   }
   /*if lives == 0 *die* */
 }
 
 class alien {
-  constructor(lives){
+  constructor(lives = 1){
     this.lives = lives;
+  }
+  draw(){
+    
   }
   /*if lives == 0 *die* */
 }
