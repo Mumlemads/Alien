8620575dd78c0286b234f3d00c67a545217a2f70
Mumlemads <mads.lauge8@gmail.com>
2023-08-23 11:20:25 +0200

    alien added

--- sketch.js
+++ sketch.js
@@ -5,21 +5,50 @@ function setup() {
   b1 = new bunker();
   b2 = new bunker();
   b3 = new bunker();
+  alien = new alien(200, 200);
+
 }
 
 function draw() {
   background(0);
+  player.draw();
+  alien.draw();
+}
+let alien0_img;
+let alien1_img;
+let ship_img;
+function preload(){
+  ship_img = loadImage('shipe.png');
+  alien0_img = loadImage('alien0.png');
+  alien1_img = loadImage('alien1.png');
 }
 
 class Ship {
-  constructor(lives = 3, x = width/2, y = 10){
+  constructor(lives = 3, x = width/2, y = 650){
     this.lives = lives;
     this.x = x;
     this.y = y;
   }
+  draw(){
+    image(ship_img, this.x - 25, this.y, 50, 50);
+    if (keyIsDown(LEFT_ARROW)){
+      this.x--
+    }
+    if (keyIsDown(RIGHT_ARROW)){
+      this.x++
+    }
+  }
+  update(){
+
+  }
   /*if lives == 0 *die* */
 
 }
+
+
+
+
+
 
 class bunker {
   constructor(lives = 40){
@@ -33,7 +62,7 @@ class alien {
     this.lives = lives;
   }
   draw(){
-    
+    image(alien0_img, 200, 200, 50, 50);
   }
   /*if lives == 0 *die* */
 }
